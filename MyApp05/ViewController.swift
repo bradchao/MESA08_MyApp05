faa0af86b1002342f952b67e6ace07eab69dcb3b
Brad <brad@brad.tw>
2017-09-18 14:21:16 +0800

    2017091801

--- MyApp05/ViewController.swift
+++ MyApp05/ViewController.swift
@@ -8,18 +8,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var btnNumber1: UIButton!
+    
+    private let items = ["iPhone","Android","Windows","iPad","Apple Watch"]
+    private let item2s = ["Item1","Item2","Item3","Item4","Item5","Item6","Item7",]
+    
 
+    @IBAction func clickNumber1(_ sender: Any) {
+        btnNumber1.isEnabled = false
+        //showDialog()
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var ret = 0;
+        if component == 0 {
+            ret = items.count
+        }else if (component == 1){
+            ret = item2s.count
+        }
+        return ret
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        var ret = ""
+        if component == 0 {
+            ret = items[row]
+        }else if component == 1 {
+            ret = item2s[row]
+        }
+        
+        return ret
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //showDialog()
+        print("viewDidLoad")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+        showDialog()
     }
-
+    
+    private func showDialog(){
+        let alert = UIAlertController(title: "Welcome", message: "World", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default){
+            (aAction) in
+            let account = alert.textFields![0].text
+            let passwd = alert.textFields![1].text
+            print("\(account!):\(passwd!)")
+        }
+        alert.addAction(action)
+        
+        alert.addTextField { (aTextField) in
+            aTextField.placeholder = "輸入帳號"
+        }
+        
+        alert.addTextField { (aTextField) in
+            aTextField.placeholder = "輸入密碼"
+            aTextField.isSecureTextEntry = true
+        }
+        
+        
+        
+        
+        show(alert, sender: self)
+        
+    }
+    
 
 }
 
